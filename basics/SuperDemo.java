1cdedcf81a5e065ab0de7dbb5b39ba5a33743d0b
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-03 14:19:56 +0000

    Super and This

---
+++ basics/SuperDemo.java
@@ -0,0 +1,25 @@
+class A {
+  public A() {
+    System.out.println("in A");
+  }
+  public A(int A) {
+    System.out.println("In A paramaterized");
+  }
+}
+
+class B extends A {
+  public B() {
+    System.out.println("In B");
+  }
+  public B(int a) {
+    //super(a);
+    this();
+    System.out.println("In B paramaterized");
+  }
+}
+
+public class SuperDemo {
+  public static void main(String[] args) {
+    B obj = new B(3);
+  }
+}
