ce7971d1a07a9fa6ef767ea5fb5a34da69cf8665
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-05 15:40:27 +0530

    Thread using Runnable

---
+++ multithreading/RunnableDemo.java
@@ -0,0 +1,29 @@
+package multithreading;
+
+class A implements Runnable{
+  public void run() {
+    for(int i=0; i<10; i++) {
+      System.out.println("Hi");   
+    }
+  } 
+}
+
+class B implements Runnable{
+  public void run() {
+    for(int i=0; i<10; i++) {
+      System.out.println("Hello");   
+    }
+  } 
+}
+public class RunnableDemo {
+  public static void main(String[] args) {
+    A obj1 = new A();
+    B obj2 = new B();
+    Thread t1 = new Thread(obj1);
+    Thread t2 = new Thread(obj2);
+
+    t1.start();
+    t2.start();
+
+  }
+}
