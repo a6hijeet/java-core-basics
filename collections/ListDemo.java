64249ad75763c034ee55e316b0ce8174c051c92b
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-05 16:59:25 +0530

    Collections - List

---
+++ collections/ListDemo.java
@@ -0,0 +1,38 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+
+public class ListDemo {
+  public static void main(String[] args) {
+    List<Integer> al = new ArrayList<>();
+    for(int i=1; i<=5; i++){
+      al.add(i);
+    }
+    al.remove(2);
+    System.out.println(al);
+
+    List<Integer> v = new Vector<>();
+    for(int i=1; i<=5; i++){
+      v.add(i);
+    }
+    v.remove(2);
+    System.out.println(v);
+
+    List<Integer> ll = new LinkedList<>();
+    for(int i=1; i<=5; i++){
+      ll.add(i);
+    }
+
+    System.out.println(ll);
+
+    List<Integer> st = new Stack<>();
+    for(int i=1; i<=5; i++){
+      st.add(i);
+    }
+    System.out.println(st);
+  }
+}
