175acf2bb037dc3dbd7d975f97cf389c69e0bcff
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-05 15:05:14 +0530

    User Input demo

--- IO/IODemo.java
+++ IO/IODemo.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 public class IODemo {
   public static void main(String[] args) throws IOException{
+
+    // Take input using BufferedReader.
     InputStreamReader in = new InputStreamReader(System.in);
     BufferedReader b = new BufferedReader(in);
     System.out.println("Using BufferedReader Enter name");
@@ -13,12 +15,14 @@ public class IODemo {
     System.out.println("Hello " + name);
     b.close();
 
+    // Take input using scanner.
     Scanner sc = new Scanner(System.in);
     System.out.println("Using Scanner Enter name");
     String userInput = sc.nextLine();
     System.out.println("Hello " + userInput);
     sc.close();
 
+    // Take multiple inputs using scanner.
     Scanner sc1 = new Scanner(System.in);
     System.out.println("User scanner Enter name");
     String username = sc1.nextLine();
@@ -27,5 +31,24 @@ public class IODemo {
 
     System.out.println("User name " + username + " : Age " + age);
     sc1.close();
+
+    // Close resource using finally.
+    BufferedReader b1 = null;
+    try {
+      b1 = new BufferedReader(new InputStreamReader(System.in));
+      System.out.println("Try Enter name");
+      String name1 = b1.readLine();
+      System.out.println("Hello " + name1);
+    }finally {
+      b1.close();
+    }
+    
+    // Using try to close resource.
+    try(BufferedReader b2 = new BufferedReader(new InputStreamReader(System.in))) {
+      System.out.println("Try Enter name");
+      String name1 = b2.readLine();
+      System.out.println("Hello " + name1);
+    }   
   }
+
 }
