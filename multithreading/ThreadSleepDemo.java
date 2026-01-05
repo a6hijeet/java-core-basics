233283baf6519224d7f8c14da8452e9e43064638
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-05 15:30:23 +0530

    Thread with sleep

---
+++ multithreading/ThreadSleepDemo.java
@@ -0,0 +1,37 @@
+package multithreading;
+
+class A extends Thread{
+  public void run() {
+    for(int i=0; i<10; i++) {
+      System.out.println("Hi"); 
+      try {
+        Thread.sleep(200);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }  
+    }
+  } 
+}
+
+class B extends Thread{
+  public void run() {
+    for(int i=0; i<10; i++) {
+      System.out.println("Hello");  
+       try {
+        Thread.sleep(200);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }   
+    }
+  } 
+}
+
+public class ThreadSleepDemo {
+  public static void main(String[] args) {
+    A obj1 = new A();
+    B obj2 = new B();
+
+    obj1.start();
+    obj2.start();
+  }
+}
