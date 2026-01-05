430471db0df2812f2028e9ead381cd50dc94edaf
a6hijeet <abhijeetsatpute.dev@gmail.com>
2026-01-05 15:28:22 +0530

    Simple thread demo

---
+++ multithreading/ThreadDemo.java
@@ -0,0 +1,27 @@
+package multithreading;
+
+class A extends Thread{
+  public void run() {
+    for(int i=0; i<10; i++) {
+      System.out.println("Hi");   
+    }
+  } 
+}
+
+class B extends Thread{
+  public void run() {
+    for(int i=0; i<10; i++) {
+      System.out.println("Hello");   
+    }
+  } 
+}
+
+public class ThreadDemo {
+  public static void main(String[] args) {
+    A obj1 = new A();
+    B obj2 = new B();
+
+    obj1.start();
+    obj2.start();
+  }
+}
